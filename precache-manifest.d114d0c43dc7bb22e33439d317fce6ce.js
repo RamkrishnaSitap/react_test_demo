1e8de51a1b0aa2f95360292b0b8df4530e77ae4d
Ramkrishna Sitap <sitapram7@gmail.com>
2020-10-15 15:07:00 +0530

    Updates

---
+++ precache-manifest.d114d0c43dc7bb22e33439d317fce6ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84505880b3f6566273da8934a3783c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd762fa5a2ce6593416b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f2781b31cef702d2f255",
+    "url": "/static/js/2.983c9493.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.983c9493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd762fa5a2ce6593416b",
+    "url": "/static/js/main.c9f73b74.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
